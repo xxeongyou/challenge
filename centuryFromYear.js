c98c10cc17afb5a7a43269881f33dd432bc1139f
xxeongyou <xxeongyou@gmail.com>
2020-12-09 20:30:10 +0900

    centry from year 4th challenge

---
+++ centuryFromYear.js
@@ -0,0 +1,40 @@
+function centuryFromYear(num) {
+    let century = Math.floor(num*0.01)+1;
+    if(num%100===0) {
+       century -= 1; 
+    }
+    return century;   
+}
+
+/*
+Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+*/
+describe('centuryFromYear()', () => {
+    it('returns current century', () => {
+        // arrange
+        const year = 1905;
+        
+        // act
+        const result = centuryFromYear(year);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(20);
+    });
+    
+    it('returns current century for edge case of start of century', () => {
+        // arrange
+        const year = 1700;
+        
+        // act
+        const result = centuryFromYear(year);
+
+        // log
+        console.log("result 2: ", result);
+        
+        // assert
+        expect(result).toBe(17);
+    });    
+});
